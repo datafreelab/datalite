5a59661fd3b83ceeb4c618e0c1095c2c6f905552
mylinyuzhi <mylinyuzhi@gmail.com>
2023-04-04 10:07:45 +0800

    support: parse datatype error

--- src/common/error/src/lib.rs
+++ src/common/error/src/lib.rs
@@ -1,2 +1,7 @@
+#![feature(trait_alias)]
+use crate::errors::BoxedError;
+
 pub mod errors;
 pub mod status;
+
+pub type Result<T> = std::result::Result<T, BoxedError>;
--- src/common/error/src/status.rs
+++ src/common/error/src/status.rs
@@ -2,17 +2,16 @@ use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct StatusCode(i32);
+pub struct Status(i32);
 
-impl StatusCode {
+impl Status {
     #[inline]
     pub fn code(&self) -> i32 {
         self.0
     }
 
-    #[allow(unconditional_recursion)]
     pub fn name(&self) -> &'static str {
-        match_name(self.0).unwrap_or("<unknown status code>")
+        name_opt(self.0).unwrap_or("<unknown status>")
     }
 
     #[inline]
@@ -21,52 +20,52 @@ impl StatusCode {
     }
 
     pub fn is_retryable(&self) -> bool {
-        match_retryable(self.0)
+        retryable_opt(self.0)
     }
 }
 
-impl Default for StatusCode {
+impl Default for Status {
     #[inline]
-    fn default() -> StatusCode {
-        StatusCode::OK
+    fn default() -> Status {
+        Status::OK
     }
 }
 
-impl Debug for StatusCode {
+impl Debug for Status {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "Status:{{{:x},{}}}", self.0, self.name())?;
         Ok(())
     }
 }
 
-impl Display for StatusCode {
+impl Display for Status {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         Debug::fmt(self, f)
     }
 }
 
-impl From<i32> for StatusCode {
+impl From<i32> for Status {
     #[inline]
-    fn from(code: i32) -> StatusCode {
-        StatusCode(code)
+    fn from(code: i32) -> Status {
+        Status(code)
     }
 }
-macro_rules! status_codes {
+macro_rules! status {
     (
         $(
             $(#[$docs:meta])*
             ($num:expr, $konst:ident, $phrase:expr, $retryable:expr);
         )+
     ) => {
-        impl StatusCode {
+        impl Status {
         $(
             $(#[$docs])*
-            pub const $konst: StatusCode = StatusCode($num);
+            pub const $konst: Status = Status($num);
         )+
 
         }
 
-        fn match_name(num: i32) -> Option<&'static str> {
+        fn name_opt(num: i32) -> Option<&'static str> {
             match num {
                 $(
                 $num => Some($phrase),
@@ -74,7 +73,7 @@ macro_rules! status_codes {
                 _ => None
             }
         }
-        fn match_retryable(num:i32) ->bool{
+        fn retryable_opt(num:i32) ->bool{
             match num {
                 $(
                 $num => $retryable,
@@ -85,21 +84,23 @@ macro_rules! status_codes {
     }
 }
 
-status_codes! {
+status! {
     (0x0000_0000, OK, "OK", true);
     (0x0000_0001, ERR_UNKNOWN, "ErrUnknown",false);
     (0x0001_0001, ERR_PARSE_DATATYPE, "ErrParseDataType",false);
     }
+
+
 #[cfg(test)]
 mod test {
-    use crate::status::StatusCode;
+    use crate::status::Status;
 
     #[test]
     fn test_status_code() {
-        assert_eq!(StatusCode::OK.code(), 0);
-        assert_eq!(StatusCode::OK.name(), "OK");
-        assert_eq!(StatusCode::ERR_UNKNOWN.code(), 0x0000_0001);
-        assert_eq!(StatusCode::ERR_UNKNOWN.is_retryable(), false);
-        assert_eq!(StatusCode::ERR_PARSE_DATATYPE.code(), 0x0001_0001);
+        assert_eq!(Status::OK.code(), 0);
+        assert_eq!(Status::OK.name(), "OK");
+        assert_eq!(Status::ERR_UNKNOWN.code(), 0x0000_0001);
+        assert_eq!(Status::ERR_UNKNOWN.is_retryable(), false);
+        assert_eq!(Status::ERR_PARSE_DATATYPE.code(), 0x0001_0001);
     }
 }
